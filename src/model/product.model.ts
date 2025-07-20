b141c082fd756d0dfc1141f2d6c2b19957c8da84
O.V.Chanuka Prabodha <ckprabodha@gmail.com>
2025-07-20 23:37:57 +0530

    add models and DTOs for Contact, Product, and User

---
+++ src/model/product.model.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const productModel =new mongoose.Schema(
+    {
+        "id": {
+            required: true,
+            type: Number,
+            unique: true,
+            index: true // For better performance on queries
+        },
+        "name": {
+            required: true,
+            type: String,
+        },
+        "price": {
+            required: true,
+            type: Number
+        },
+        "currency": {
+            required: true,
+            type: String
+        },
+        "image": {
+            required: true,
+            type: String
+        }
+    }
+);
+
+const Product
+    = mongoose.model('Product', productModel);
+
+export default Product;
